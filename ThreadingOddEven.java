98c7d4bc218bb908f13e3138a10eec66a513fa48
Vibhu0112 <90963776+Vibhu0112@users.noreply.github.com>
2023-02-22 22:22:10 +0530

    Add files via upload

---
+++ ThreadingOddEven.java
@@ -0,0 +1,28 @@
+public class ThreadingOddEven {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @ Override
+            public void run() {
+                for (int i = 0;i<= 10;i++) {
+                    if (i % 2 == 0) {
+                        System.out.println("Even number: " + i);
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @ Override
+            public void run() {
+                for (int i = 0;i<= 10;i++) {
+                    if (i % 2 != 0) {
+                        System.out.println("Odd number: " + i);
+                    }
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
